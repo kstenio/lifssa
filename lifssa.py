25dec2a47079c46e8697fdb603669a2be7a6eef9
Kleydson Stenio <kleydson.stenio@gmail.com>
2021-06-28 19:31:06 -0300

    Added file output.txt (plot template) Created connect for spectra select push button Created openfile method - Call a QFileDialog (getopnefilename) - Do plot

--- lifssa.py
+++ lifssa.py
@@ -1,5 +1,7 @@
 # Imports
 from PyQt5 import QtWidgets, uic
+from pathlib import Path
+import pandas as pd
 
 # Main class/GUI application
 class LIFSSA(QtWidgets.QMainWindow):
@@ -7,6 +9,16 @@ class LIFSSA(QtWidgets.QMainWindow):
 		super(LIFSSA, self).__init__()
 		self.gui = uic.loadUi('lifssa.ui')
 		self.gui.show()
+		# Connects
+		self.gui.spectraselect_pb.clicked.connect(self.openfile)
+	
+	def openfile(self):
+		file = QtWidgets.QFileDialog.getOpenFileName(self, 'Select spectrum file', str(Path.cwd()), 'Text files (*.txt)')
+		if file[0] == '':
+			QtWidgets.QMessageBox.warning(None, 'Warning', 'Cancelled by the user')
+		else:
+			matrix = pd.read_csv(file[0], header=None, sep=' ').to_numpy()
+			self.gui.graphic.plot(matrix[:, 0], matrix[:, 1])
 
 # Main starter
 if __name__ == '__main__':
